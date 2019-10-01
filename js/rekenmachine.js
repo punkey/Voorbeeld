aa9c35c0f0e26dcd53ab8877cd2ca174e15d5d0c
punkey <55575590+punkey@users.noreply.github.com>
2019-10-01 09:09:40 +0200

    Initial commit

---
+++ js/rekenmachine.js
@@ -0,0 +1,48 @@
+var getal1 = document.getElementById("getal1");
+var getal2 = document.getElementById("getal2");
+
+var btnSom = document.getElementById("btnSom");
+var btnVerschil = document.getElementById("btnVerschil");
+var btnProduct = document.getElementById("btnProduct");
+var btnQuotient = document.getElementById("btnQuotient");
+
+var uitvoer = document.getElementById("uitvoer")
+
+btnSom.addEventListener("click", function() {
+    let resultaat = parseInt(getal1.value) + parseInt(getal2.value); 
+    let paragraph = createParagraph();
+    paragraph.innerHTML = "De som van" + getal1.value + " en " +getal2.value + " is " + resultaat;
+    uitvoer.appendChild(paragraph)
+});
+
+btnVerschil.addEventListener("click", function() {
+    let resultaat = parseInt(getal1.value) - parseInt(getal2.value); 
+    let paragraph = createParagraph();
+    paragraph.innerHTML = "De Verschil van" + getal1.value + " en " +getal2.value + " is " + resultaat;
+    uitvoer.appendChild(paragraph)
+});
+
+btnProduct.addEventListener("click", function() {
+    let resultaat = parseInt(getal1.value) * parseInt(getal2.value); 
+    let paragraph = createParagraph();
+    paragraph.innerHTML = "De Product van" + getal1.value + " en " +getal2.value + " is " + resultaat;
+    uitvoer.appendChild(paragraph)
+});
+
+btnQuotient.addEventListener("click", function() {
+    let resultaat = parseInt(getal1.value) / parseInt(getal2.value); 
+    let paragraph = createParagraph();
+    paragraph.innerHTML = "De Quotient van" + getal1.value + " en " +getal2.value + " is " + resultaat;
+    uitvoer.appendChild(paragraph)
+});
+
+function createParagraph() {
+    let paragraph = document.createElement("p");
+    
+    return paragraph;     
+}
+
+
+
+
+
